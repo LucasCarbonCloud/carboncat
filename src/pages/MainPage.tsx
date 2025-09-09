41f00f4735a78dbda18b5d1a191f6b6665f85896
Lucas Börjesson <lucas.borjesson@carboncloud.com>
2025-09-09 14:15:10 +0200

    work

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Field, TimeRange } from '@grafana/data';
-import { TimeRangePicker, useTheme2 } from '@grafana/ui';
+import { Field, rangeUtil, TimeRange } from '@grafana/data';
+import { RefreshPicker, TimeRangePicker, useTheme2 } from '@grafana/ui';
 // import { prefixRoute } from '../utils/utils.routing';
 // import { ROUTES } from '../constants';
 // import { testIds } from '../components/testIds';
 import '../style.js';
-import { runListApps, runListComponents, runListTeams, runLogQuery} from 'utils/clickhouse';
+import { runListApps, runListComponents, runListTeams, runLogQuery } from 'utils/clickhouse';
 import { Filter, FilterOperation, HighLevelFilter, SimpleOptions } from 'types/filters';
 import { getFieldNames } from 'utils/functions';
 import clsx from 'clsx';
@@ -16,6 +16,7 @@ import { Settings } from 'components/Settings';
 import { Searchbar } from 'components/Searchbar';
 import { DATASOURCES, getQVar, getQVarTimeRange, setQVar, setQVarTimeRange } from 'utils/variables';
 import ToggleButtonGroup from 'components/Components';
+
 
 function PageOne() {
   const theme = useTheme2();
@@ -35,56 +36,71 @@ function PageOne() {
 
   const [selectedLabels, setSelectedLabels] = useState<string[]>(getQVar('labels'));
   const [selectedFields, setSelectedFields] = useState<string[]>(getQVar('fields'));
-  const [selectedFilters, setSelectedFilters] = useState<Filter[]>(getQVar("filters"));
+  const [selectedFilters, setSelectedFilters] = useState<Filter[]>(getQVar('filters'));
   // const [showLevel, setShowLevel] = useState<boolean>(false);
   const [tableLineHeight, setTableLineHeight] = useState<number>(35);
-  const [searchTerm, setSearchTerm] = useState<string>(getQVar("searchTerm"));
-  const [filteredSearchTerm, setFilteredSearchTerm] = useState<string>(getQVar("searchTerm"));
+  const [searchTerm, setSearchTerm] = useState<string>(getQVar('searchTerm'));
+  const [filteredSearchTerm, setFilteredSearchTerm] = useState<string>(getQVar('searchTerm'));
   const [logDetails, setLogDetails] = useState<number | undefined>(undefined);
   const [timeRange, setTimeRange] = useState<TimeRange>(getQVarTimeRange());
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string|undefined>(undefined);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
-  useEffect(() => {
+  const refreshData = () => {
     setIsLoading(true);
     Promise.all([
-      runLogQuery(datasource, timeRange, filteredSearchTerm, selectedFilters, apps, logLevels, components, teams, setFields),
-      runListApps(datasource, timeRange, filteredSearchTerm, selectedFilters, setAvailableApps),
-      runListComponents(datasource, timeRange, filteredSearchTerm, selectedFilters, apps, setAvailableComponents),
-      runListTeams(datasource, timeRange, filteredSearchTerm, selectedFilters, setAvailableTeams)
-    ]).catch((r: any) => {
-      setErrorMessage(r.message)
-    }).finally(() => {
-      setIsLoading(false);
-    });
-  }, [datasource, timeRange, filteredSearchTerm, selectedFilters, apps, logLevels, components, teams])
+      runLogQuery(
+        datasource,
+        rangeUtil.convertRawToRange(timeRange.raw),
+        filteredSearchTerm,
+        selectedFilters,
+        apps,
+        logLevels,
+        components,
+        teams,
+        setFields
+      ),
+      runListApps(datasource, rangeUtil.convertRawToRange(timeRange.raw), filteredSearchTerm, selectedFilters, setAvailableApps),
+      runListComponents(datasource, rangeUtil.convertRawToRange(timeRange.raw), filteredSearchTerm, selectedFilters, apps, setAvailableComponents),
+      runListTeams(datasource, rangeUtil.convertRawToRange(timeRange.raw), filteredSearchTerm, selectedFilters, setAvailableTeams),
+    ])
+      .catch((r: any) => {
+        setErrorMessage(r.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-
+  useEffect(() => {
+    refreshData();
+  }, [datasource, timeRange, filteredSearchTerm, selectedFilters, apps, logLevels, components, teams]);
 
   const handleFieldChange = (value: string[], type: string) => {
     if (type === 'label') {
       setSelectedLabels(value);
-      setQVar('labels', value)
+      setQVar('labels', value);
     } else if (type === 'field') {
       setSelectedFields(value);
-      setQVar("fields", value)
+      setQVar('fields', value);
     }
   };
 
   const handleSearchTermChange = (value: string) => {
     setSearchTerm(value);
     // remove #... tokens (and trailing space if any)
-    const filteredVal = value.replace(/#\S*\s?/g, "").trim();
-    setFilteredSearchTerm(filteredVal)
-    setQVar("searchTerm", filteredVal)
+    const filteredVal = value.replace(/#\S*\s?/g, '').trim();
+    setFilteredSearchTerm(filteredVal);
+    setQVar('searchTerm', filteredVal);
   };
 
   const handleTimeRangeChange = (value: TimeRange) => {
-    setTimeRange(value)
-    setQVarTimeRange(value)
+    console.log(timeRange)
+    setTimeRange(value);
+    setQVarTimeRange(value);
   };
 
-  const handleTableLineHeight= (value: number) => {
+  const handleTableLineHeight = (value: number) => {
     setTableLineHeight(value);
     // locationService.partial({ 'var-tableLineHeight': value }, true);
   };
@@ -103,8 +119,8 @@ function PageOne() {
         return prevFilters;
       }
 
-      if ( op === "only" ) {
-        return [{ key, operation, value }]
+      if (op === 'only') {
+        return [{ key, operation, value }];
       }
 
       const exists = prevFilters.some((f) => f.key === key && f.operation === operation && f.value === value);
@@ -115,11 +131,10 @@ function PageOne() {
         return [...prevFilters, { key, operation, value }];
       }
     };
-    const newFilters = filters(selectedFilters)
-    setSelectedFilters(newFilters)
-    setQVar("filters", newFilters)
+    const newFilters = filters(selectedFilters);
+    setSelectedFilters(newFilters);
+    setQVar('filters', newFilters);
   };
-
 
   let labels: string[] = [];
 
@@ -140,12 +155,12 @@ function PageOne() {
   labels = labels.sort();
 
   const options: SimpleOptions = {
-  traceUrl:"",
-  text:"",
-}
+    traceUrl: '',
+    text: '',
+  };
 
   const hlFilters: HighLevelFilter = {
-    availableLogLevels: ["DEBUG", "INFO", "WARN", "ERROR", "FATAL"],
+    availableLogLevels: ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'],
     logLevels: logLevels,
     setLogLevels: setLogLevels,
     availableApps: availableApps,
@@ -156,8 +171,8 @@ function PageOne() {
     setComponents: setComponents,
     availableTeams: availableTeams,
     teams: teams,
-    setTeams:setTeams,
-  }
+    setTeams: setTeams,
+  };
 
   return (
     <div className={`flex h-full w-full gap-2 px-2 pt-4 relative max-h-[calc(100dvh-50px)] `}>
@@ -176,28 +191,31 @@ function PageOne() {
       </div>
       <div className="flex flex-col flex-grow gap-4 pr-2 m-2">
         <div className="flex items-center">
-        <Searchbar
-          searchTerm={searchTerm}
-          fields={fields}
-          labels={[...keys, ...labels]}
-          onChange={handleSearchTermChange}
-          selectedFilters={selectedFilters}
-          setSelectedFilters={handleSetFilterTerm}
-        />
-        <ToggleButtonGroup
-          defaultValue= {DATASOURCES[0].value}
-          options={DATASOURCES}
-          onChange={setDatasource}
-        />
-        <TimeRangePicker
-          value={timeRange}
-          onChange={handleTimeRangeChange}
-          onChangeTimeZone={() => {}}
-          onMoveBackward={() => {}}
-          onMoveForward={() => {}}
-          onZoom={() => {}}
-        />
+          <Searchbar
+            searchTerm={searchTerm}
+            fields={fields}
+            labels={[...keys, ...labels]}
+            onChange={handleSearchTermChange}
+            selectedFilters={selectedFilters}
+            setSelectedFilters={handleSetFilterTerm}
+          />
+          <ToggleButtonGroup defaultValue={DATASOURCES[0].value} options={DATASOURCES} onChange={setDatasource} />
+          <TimeRangePicker
+            value={timeRange}
+            onChange={handleTimeRangeChange}
+            onChangeTimeZone={() => {}}
+            onMoveBackward={() => {}}
+            onMoveForward={() => {}}
+            onZoom={() => {}}
+          />
+          <RefreshPicker
+            value="30s"
+            intervals={['5s', '10s', '30s', '1m', '5m', '15m', '30m', '1h', '2h', '1d']}
+            onRefresh={refreshData}
+            onIntervalChanged={() => {}}
+          />
         </div>
+
         <div className="relative flex-grow">
           <Table
             options={options}
@@ -209,29 +227,34 @@ function PageOne() {
             setLogDetails={handleSetLogDetails}
           />
           {isLoading && (
-            <div className={clsx(
-              "flex absolute inset-0 z-10 justify-center items-center rounded-lg backdrop-blur-[3px]",
-              theme.isDark ? 'bg-black/20' : 'bg-white/20'
-            )}>
+            <div
+              className={clsx(
+                'flex absolute inset-0 z-10 justify-center items-center rounded-lg backdrop-blur-[3px]',
+                theme.isDark ? 'bg-black/20' : 'bg-white/20'
+              )}
+            >
               <div className="w-12 h-12 rounded-full border-4 animate-spin border-[#28A0A6] border-t-transparent"></div>
             </div>
           )}
           {errorMessage !== undefined && (
-            <div className={clsx(
-              "flex absolute inset-0 z-10 justify-center items-center rounded-lg backdrop-blur-[3px]",
-              theme.isDark ? 'bg-black/20' : 'bg-white/20'
-            )}>
+            <div
+              className={clsx(
+                'flex absolute inset-0 z-10 justify-center items-center rounded-lg backdrop-blur-[3px]',
+                theme.isDark ? 'bg-black/20' : 'bg-white/20'
+              )}
+            >
               <div className="p-4 w-1/2 h-80 font-semibold text-white bg-red-700 rounded-lg shadow-md">
-                { errorMessage }
+                {errorMessage}
               </div>
             </div>
           )}
         </div>
       </div>
-      {logDetails !== undefined && <LogDetails options={options} fields={fields} rowIndex={logDetails} setLogDetails={handleSetLogDetails} />}
+      {logDetails !== undefined && (
+        <LogDetails options={options} fields={fields} rowIndex={logDetails} setLogDetails={handleSetLogDetails} />
+      )}
     </div>
   );
 }
 
 export default PageOne;
-
